25830130f7986e2fda6930da85d2c102d89dd2a8
kalyani11git <166929600+kalyani11git@users.noreply.github.com>
2025-01-14 16:20:42 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb://kalyanimali:kalyani123 @localhost:27017/mern_numetry_db?authSource=admin", { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected...'))
   .catch(err => console.log(err));
