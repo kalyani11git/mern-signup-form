aef6c0437487bdee1ecd33e229696aaba0f58c1c
kalyani11git <166929600+kalyani11git@users.noreply.github.com>
2025-01-15 17:46:56 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://rcpitwork11:kalyanimali11@cluster0.liobdhs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://rcpitwork11:kalyanimali11@cluster0.liobdhs.mongodb.net/?retryWrites=true&w=majority&tls=true";
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
