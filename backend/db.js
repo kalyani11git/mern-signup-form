a4fb549615813fda7b63ab79889571b2c73e890e
kalyani11git <166929600+kalyani11git@users.noreply.github.com>
2025-01-20 17:00:04 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 3b68b3a39edab479606ea4585870108e0e0d3f07
 const uri = process.env.DATABASE_URL;
 mongoose.connect(uri, { dbName: 'mern_app' })
   .then(() => {
