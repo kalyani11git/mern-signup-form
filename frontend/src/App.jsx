5eea1a60d57655bbe9446ab7f43a65d675bbb3e4
kalyani11git <166929600+kalyani11git@users.noreply.github.com>
2025-01-20 18:24:50 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
-// import TextSection from "./components/TextSection";
+
 import FormSection from "./components/FormSection";
 
 function App() {
   return (
     <div className="app1">
-      {/* <TextSection /> */}
+   
       <FormSection />
     </div>
   );
