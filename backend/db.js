663d2adc4ec8a2ce59b41fff29cc136257d23314
kalyani11git <166929600+kalyani11git@users.noreply.github.com>
2025-01-21 13:23:48 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const uri = process.env.DATABASE_URL;
-  // "mongodb+srv://rcpitwork11:kalyanimali11@cluster0.liobdhs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-console.log(process.env.DATABASE_URL);
+ 
+
 mongoose.connect(uri, { dbName: 'mern_app' })
   .then(() => {
     console.log('Connected to MongoDB');
