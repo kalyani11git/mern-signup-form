54b02cf2f684cd0c203d9ec8e84d439a724c02e3
kalyani11git <166929600+kalyani11git@users.noreply.github.com>
2025-01-21 12:55:46 +0530

    Update CloudinaryConfig.js

--- backend/CloudinaryConfig.js
+++ backend/CloudinaryConfig.js
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary').v2;
 
 // Configure Cloudinary
 cloudinary.config({
-  cloud_name: 'dm2gw7sur', // Replace with your Cloudinary cloud name
-  api_key: '673232598197731',       // Replace with your Cloudinary API key
-  api_secret: 'Djy9Ugoz1ug0MiNibX3lTs-AbwQ', // Replace with your Cloudinary API secret
+  cloud_name:   process.env.CLOUD_NAME, //'dm2gw7sur', // Replace with your Cloudinary cloud name
+  api_key: process.env.API_KEY,// '673232598197731',       // Replace with your Cloudinary API key
+  api_secret: process.env.API_SECRET, //'Djy9Ugoz1ug0MiNibX3lTs-AbwQ', // Replace with your Cloudinary API secret
 });
 
 module.exports = cloudinary;
